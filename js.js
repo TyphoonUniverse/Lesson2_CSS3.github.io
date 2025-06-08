5c17002dadaeaa741b77f9ec81b0b25549b9ffa6
Ngoc Nguyen Minh <168720669+TyphoonUniverse@users.noreply.github.com>
2025-06-08 16:30:26 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+function upDate(previewPic) {
+      const imageDiv = document.getElementById("image");
+      imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
+      imageDiv.innerHTML = previewPic.alt;
+    }
+
+    function unDo() {
+      const imageDiv = document.getElementById("image");
+      imageDiv.style.backgroundImage = "url('')";
+      imageDiv.innerHTML = "Hover over an image below to display here.";
+    }
